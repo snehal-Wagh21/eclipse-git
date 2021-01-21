290b666f15cdaf51a4fa45ed967fc4cbd1ae6f4a
snehal wagh <iamwaghsnehal@gmail.com>
2021-01-22 04:34:10 +0530

    rectified db errors

--- src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -2,8 +2,9 @@ package com.luv2code.springboot.thymeleafdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ThymeleafdemoApplication {
 
 	public static void main(String[] args) {
